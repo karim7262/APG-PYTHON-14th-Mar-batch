330dda15953a4a7d62e09c3319e7f4282993238d
mujahed85 <mujahed.mca@gmail.com>
2021-03-28 10:37:06 +0530

    Python Operators 28Mar

---
+++ 28Mar_Operators/02.BO_Arith.py
@@ -0,0 +1,22 @@
+a=10
+b=2
+
+#Arithmetic Operators
+c=a+b
+print(c)
+c=a-b
+print(c)
+c=a*b
+print(c)
+c=a/b
+print(c)
+
+c=a**b
+print("Power a of b is : "+str(c))
+c=a//b
+print("Integer Floor Division is: "+str(c))
+a=10.0
+c=a//b
+print("Second Time : Integer Floor Division is: "+str(c))
+
+
--- test.py
+++ test.py
@@ -3,3 +3,28 @@ s1=125
 seconds_in_a_day=20
 seconds_in_a_week = 7 * seconds_in_a_day
 print(seconds_in_a_week)
+
+# parent class
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+    def whoisThis(self):
+        print("Bird")
+    def swim(self):
+        print("Swim faster")
+
+# child class
+class Penguin(Bird):
+    def __init__(self):
+        # call super() function
+        # super(self).__init__()
+        print("Penguin is ready")
+    def whoisThis(self):				#Function Overriding
+        print("Penguin")
+    def run(self):
+        print("Run faster")
+
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
