d05705bc6fcfde07728c9c50c692286a70a10b29
mujahed85 <mujahed.mca@gmail.com>
2021-03-28 11:17:25 +0530

    Relational, Logical Operators

--- 28Mar_Operators/02.BO_Arith.py
+++ 28Mar_Operators/02.BO_Arith.py
@@ -11,8 +11,18 @@ print(c)
 c=a/b
 print(c)
 
+c="ABC"*5
+print(c)
+
+#Adv. Arithemetic Operators
 c=a**b
 print("Power a of b is : "+str(c))
+x=10
+y=5
+z=x**y # 10X10X10X10X10
+print(z)
+
+
 c=a//b
 print("Integer Floor Division is: "+str(c))
 a=10.0
---
+++ 28Mar_Operators/03.BO_RelationalOperators.py
@@ -0,0 +1,28 @@
+a=10
+b=2
+c= a!=b
+print(c)
+c=a<b
+print(c)
+c=a>b
+print(c)
+
+a=20
+b=20
+c=a<=b
+print(c) 
+
+print("Logical Operators")
+#Logical Operators
+c=True and True and True and 10==10 and 10!=20 and 1!=0 and 5*5
+print(c)
+
+c=20.23<=20.23
+print(c)
+# c= a!=b and a>b
+# print(c)
+
+# c= True and b<a and a==10 and a>b and a!=b
+# print(c)
+
+# c= False or True
